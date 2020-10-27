480079564c42c2cd2074fc9a2cae3632e9c6cabb
Jing Palma <jpalma17@outlook.com>
2020-10-28 00:39:28 +0800

    Initial commit - create project

---
+++ entity-framework-console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace entity_framework_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
